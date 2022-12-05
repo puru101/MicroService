0ab5769575017a39718518e83925ccfbeb65639c
biswa <biswalhere@gmail.com>
2022-12-05 10:10:05 +0530

    test

--- PhotoAppApiUsers/src/main/java/com/ms/user/pit/data/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ms.user.pit.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersRepository extends CrudRepository<UserEntity, Long> {
-	UserEntity findByEmail(String email);
-	//UserEntity findByUserId(String userId);
-}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MicroService
+#Test
